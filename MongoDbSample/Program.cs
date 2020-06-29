cf0ddc73b476dd0768c3c4ca692ff29bb786a9f8
cemozguraA <55392106+cemozguraA@users.noreply.github.com>
2020-06-29 06:21:43 +0300

    Add files via upload

---
+++ MongoDbSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using MongoDbSample.Helpers;
+using MongoDbSample.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MongoDbSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MongoDbHelper("TestDB");
+            db.Insert("Users", new User
+            {
+                FirstName = "Cem Özgür",
+                LastName = "Aydın",
+                Languages = new List<string>() {
+                     "c#","js","sql","xam"
+                 },
+                SchoolDetail = new School
+                {
+                    City = "Adana",
+                    Name = "AKL",
+                    Zipcode = "1360"
+                }
+            }).Wait();
+            var datas = db.LoadDatas<User>("Users");
+            foreach (var item in datas.Result)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+        }
+    }
+}
